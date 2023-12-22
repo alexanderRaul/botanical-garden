b186fca01ea92f73017f75bed18be8a50b262525
alexanderRaul <202002303@est.umss.edu>
2023-12-22 09:28:15 +0100

    set database with seed.js

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,30 +1,165 @@
+//const { db } = require('@vercel/postgres');
+//const { blogPosts, events, trees } = require('../src/app/lib/placeholder-data.js');
+//
+//const client = new db.Client({
+//  connectionString: process.env.POSTGRES_URL,
+//  ssl: {
+//    rejectUnauthorized: false,
+//  },
+//});
+//
+//async function seedBlogPosts(client) {
+//  try {
+//    await client.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+//
+//    await client.query(`
+//      CREATE TABLE IF NOT EXISTS blog_posts (
+//        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+//        title VARCHAR(255) NOT NULL,
+//        content TEXT NOT NULL,
+//        publication_date DATE NOT NULL
+//      );
+//    `);
+//
+//    console.log('Created "blog_posts" table');
+//
+//    for (const post of blogPosts) {
+//      await client.query(`
+//        INSERT INTO blog_posts (id, title, content, publication_date)
+//        VALUES ($1, $2, $3, $4)
+//        ON CONFLICT (id) DO NOTHING;
+//      `, [post.id, post.title, post.content, post.publication_date]);
+//    }
+//
+//    console.log(`Seeded ${blogPosts.length} blog posts`);
+//  } catch (error) {
+//    console.error('Error seeding blog posts:', error);
+//    throw error;
+//  }
+//}
+//
+//async function seedEvents(client) {
+//  try {
+//    await client.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+//
+//    await client.query(`
+//      CREATE TABLE IF NOT EXISTS events (
+//        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+//        title VARCHAR(255) NOT NULL,
+//        description TEXT NOT NULL,
+//        date DATE NOT NULL
+//      );
+//    `);
+//
+//    console.log('Created "events" table');
+//
+//    for (const event of events) {
+//      await client.query(`
+//        INSERT INTO events (id, title, description, date)
+//        VALUES ($1, $2, $3, $4)
+//        ON CONFLICT (id) DO NOTHING;
+//      `, [event.id, event.title, event.description, event.date]);
+//    }
+//
+//    console.log(`Seeded ${events.length} events`);
+//  } catch (error) {
+//    console.error('Error seeding events:', error);
+//    throw error;
+//  }
+//}
+//
+//async function seedTrees(client) {
+//  try {
+//    await client.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+//
+//    await client.query(`
+//      CREATE TABLE IF NOT EXISTS trees (
+//        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+//        scientific_name VARCHAR(255) NOT NULL,
+//        common_name VARCHAR(255) NOT NULL,
+//        image_url VARCHAR(255) NOT NULL
+//      );
+//    `);
+//
+//    console.log('Created "trees" table');
+//
+//    for (const tree of trees) {
+//      await client.query(`
+//        INSERT INTO trees (id, scientific_name, common_name, image_url)
+//        VALUES ($1, $2, $3, $4)
+//        ON CONFLICT (id) DO NOTHING;
+//      `, [tree.id, tree.scientific_name, tree.common_name, tree.image_url]);
+//    }
+//
+//    console.log(`Seeded ${trees.length} trees`);
+//  } catch (error) {
+//    console.error('Error seeding trees:', error);
+//    throw error;
+//  }
+//}
+//
+//
+//async function main() {
+//  const client = new db.Client({
+//    connectionString: process.env.POSTGRES_URL,
+//    ssl: {
+//      rejectUnauthorized: false,
+//    },
+//  });
+//
+//  try {
+//    await client.connect();
+//
+//    await seedBlogPosts(client);
+//    await seedEvents(client);
+//    await seedTrees(client);
+//  } catch (error) {
+//    console.error('An error occurred while seeding the database:', error);
+//  } finally {
+//    await client.end();
+//  }
+//}
+//
+//main().catch((err) => {
+//  console.error('An error occurred in the main function:', err);
+//});
+
+
+
 const { db } = require('@vercel/postgres');
 const { blogPosts, events, trees } = require('../src/app/lib/placeholder-data.js');
 
 async function seedBlogPosts(client) {
   try {
-    await client.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
-
     await client.query(`
       CREATE TABLE IF NOT EXISTS blog_posts (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         title VARCHAR(255) NOT NULL,
         content TEXT NOT NULL,
         publication_date DATE NOT NULL
       );
     `);
 
-    console.log('Created "blog_posts" table');
+    console.log(`Created "blog_posts" table`);
 
-    for (const post of blogPosts) {
-      await client.query(`
-        INSERT INTO blog_posts (id, title, content, publication_date)
-        VALUES ($1, $2, $3, $4)
-        ON CONFLICT (id) DO NOTHING;
-      `, [post.id, post.title, post.content, post.publication_date]);
-    }
+    const insertedBlogPosts = await Promise.all(
+      blogPosts.map(async (post) => {
+        return client.query(
+          `
+          INSERT INTO blog_posts (title, content, publication_date)
+          VALUES ($1, $2, $3)
+          ON CONFLICT (id) DO NOTHING;
+        `,
+          [post.title, post.content, post.publication_date]
+        );
+      })
+    );
 
-    console.log(`Seeded ${blogPosts.length} blog posts`);
+    console.log(`Seeded ${insertedBlogPosts.length} blog posts`);
+
+    return {
+      blogPosts: insertedBlogPosts,
+    };
   } catch (error) {
     console.error('Error seeding blog posts:', error);
     throw error;
@@ -33,30 +168,35 @@ async function seedBlogPosts(client) {
 
 async function seedEvents(client) {
   try {
-    await client.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
-
-    // Crea la tabla "events" si aún no existe
     await client.query(`
       CREATE TABLE IF NOT EXISTS events (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         title VARCHAR(255) NOT NULL,
         description TEXT NOT NULL,
         date DATE NOT NULL
       );
     `);
 
-    console.log('Created "events" table');
+    console.log(`Created "events" table`);
 
-    // Inserta datos en la tabla "events"
-    for (const event of events) {
-      await client.query(`
-        INSERT INTO events (id, title, description, date)
-        VALUES ($1, $2, $3, $4)
-        ON CONFLICT (id) DO NOTHING;
-      `, [event.id, event.title, event.description, event.date]);
-    }
+    const insertedEvents = await Promise.all(
+      events.map(async (event) => {
+        return client.query(
+          `
+          INSERT INTO events (title, description, date)
+          VALUES ($1, $2, $3)
+          ON CONFLICT (id) DO NOTHING;
+        `,
+          [event.title, event.description, event.date]
+        );
+      })
+    );
 
-    console.log(`Seeded ${events.length} events`);
+    console.log(`Seeded ${insertedEvents.length} events`);
+
+    return {
+      events: insertedEvents,
+    };
   } catch (error) {
     console.error('Error seeding events:', error);
     throw error;
@@ -65,30 +205,35 @@ async function seedEvents(client) {
 
 async function seedTrees(client) {
   try {
-    await client.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
-
-    // Crea la tabla "trees" si aún no existe
     await client.query(`
       CREATE TABLE IF NOT EXISTS trees (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         scientific_name VARCHAR(255) NOT NULL,
         common_name VARCHAR(255) NOT NULL,
         image_url VARCHAR(255) NOT NULL
       );
     `);
 
-    console.log('Created "trees" table');
+    console.log(`Created "trees" table`);
 
-    // Inserta datos en la tabla "trees"
-    for (const tree of trees) {
-      await client.query(`
-        INSERT INTO trees (id, scientific_name, common_name, image_url)
-        VALUES ($1, $2, $3, $4)
-        ON CONFLICT (id) DO NOTHING;
-      `, [tree.id, tree.scientific_name, tree.common_name, tree.image_url]);
-    }
+    const insertedTrees = await Promise.all(
+      trees.map(async (tree) => {
+        return client.query(
+          `
+          INSERT INTO trees (scientific_name, common_name, image_url)
+          VALUES ($1, $2, $3)
+          ON CONFLICT (id) DO NOTHING;
+        `,
+          [tree.scientific_name, tree.common_name, tree.image_url]
+        );
+      })
+    );
 
-    console.log(`Seeded ${trees.length} trees`);
+    console.log(`Seeded ${insertedTrees.length} trees`);
+
+    return {
+      trees: insertedTrees,
+    };
   } catch (error) {
     console.error('Error seeding trees:', error);
     throw error;
@@ -96,27 +241,18 @@ async function seedTrees(client) {
 }
 
 async function main() {
-  const client = new db.Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+  const client = await db.connect();
 
-  try {
-    await client.connect();
+  await seedBlogPosts(client);
+  await seedEvents(client);
+  await seedTrees(client);
 
-    await seedBlogPosts(client);
-    await seedEvents(client);
-    await seedTrees(client);
-  } catch (error) {
-    console.error('An error occurred while seeding the database:', error);
-  } finally {
-    await client.end();
-  }
+  await client.end();
 }
 
 main().catch((err) => {
-  console.error('An error occurred in the main function:', err);
+  console.error(
+    'An error occurred while attempting to seed the database:',
+    err,
+  );
 });
-
